23c8dd37c6a7b400586fdabca93d962bb8440b47
lzx <3263760852@qq.com>
2025-08-24 00:18:24 +0800

    add velocity task

---
+++ trackerLab/tasks/velocity/robots/g1/29dof/__init__.py
@@ -0,0 +1,21 @@
+import gymnasium as gym
+
+gym.register(
+    id="Unitree-G1-29dof-Velocity",
+    entry_point="trackerLab.tracker_env:ManagerBasedTrackerEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.velocity_env_cfg:RobotEnvCfg",
+        "rsl_rl_cfg_entry_point": f"trackerLab.tasks.velocity.agents.rsl_rl_ppo_cfg:BasePPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Unitree-G1-29dof-Velocity-Play",
+    entry_point="trackerLab.tracker_env:ManagerBasedTrackerEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.velocity_env_cfg:RobotPlayEnvCfg",
+        "rsl_rl_cfg_entry_point": f"trackerLab.tasks.velocity.agents.rsl_rl_ppo_cfg:BasePPORunnerCfg",
+    },
+)
