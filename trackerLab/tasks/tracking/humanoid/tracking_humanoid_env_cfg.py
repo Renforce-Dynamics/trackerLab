288241fece717140a3c01f675818e59a5d23da04
Zaterval <ziang_zheng@foxmail.com>
2025-08-24 13:08:00 +0800

    restruct robot folder

--- trackerLab/tasks/tracking/humanoid/tracking_humanoid_env_cfg.py
+++ trackerLab/tasks/tracking/humanoid/tracking_humanoid_env_cfg.py
@@ -1,6 +1,18 @@
 from isaaclab.utils import configclass
 from trackerLab.tracker_env.manager_based_tracker_env import ManagerBasedTrackerEnvCfg
 
+from trackerLab.tracker_env.manager_based_tracker_env.manager_based_tracker_env_cfg.configs import (
+    RewardsCfg,
+    TerminationsCfg,
+    ObservationsCfg
+)
+
+@configclass
+class HumanoidTerminationCfg(TerminationsCfg):
+    def __post_init__(self):
+        return
+
+
 @configclass
 class TrackingHumanoidEnvCfg(ManagerBasedTrackerEnvCfg):
-    pass
+    terminations = HumanoidTerminationCfg()
