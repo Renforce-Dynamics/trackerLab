6d0c0774c47e21faddba6e93888e3bbf834e3256
lzx <3263760852@qq.com>
2025-09-06 21:26:12 +0800

    update max_iterations

--- trackerLab/tasks/tracking/humanoid/robots/k1/agents/rsl_rl_ppo_cfg.py
+++ trackerLab/tasks/tracking/humanoid/robots/k1/agents/rsl_rl_ppo_cfg.py
@@ -4,22 +4,22 @@ from trackerLab.tasks.tracking.agents.rsl_rl_ppo_cfg import BasePPORunnerCfg
 @configclass
 class Booster_K1_Walk_PPOCfg(BasePPORunnerCfg):
     experiment_name = "tracking_booster_k1_walk"
-    max_iterations = 10000
+    max_iterations = 7500
     
 @configclass
 class Booster_K1_Walk_Full_PPOCfg(BasePPORunnerCfg):
     experiment_name = "tracking_booster_k1_walk_full"
-    max_iterations = 15000
+    max_iterations = 7500
 
 @configclass
 class Booster_K1_Walk_Full_Deploy_PPOCfg(BasePPORunnerCfg):
     experiment_name = "tracking_booster_k1_walk_full_deploy"
-    max_iterations = 15000
+    max_iterations = 7500
 
 @configclass
 class Booster_K1_Walk_Full_Deploy_WithoutHistory_PPOCfg(BasePPORunnerCfg):
     experiment_name = "tracking_booster_k1_walk_full_deploy_without_history"
-    max_iterations = 15000
+    max_iterations = 7500
 
 @configclass
 class Booster_K1_Run_PPOCfg(BasePPORunnerCfg):
