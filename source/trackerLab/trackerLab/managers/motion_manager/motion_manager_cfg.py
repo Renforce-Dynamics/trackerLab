04ac1e1e6152e98a8e06bc085f10910b7e9b3860
Zaterval <intervalallen@gmail.com>
2025-09-07 13:29:29 +0800

    Runable with out isaaclab

--- source/trackerLab/trackerLab/managers/motion_manager/motion_manager_cfg.py
+++ source/trackerLab/trackerLab/managers/motion_manager/motion_manager_cfg.py
@@ -1,16 +1,16 @@
 from trackerLab.utils import configclass
 from trackerLab.motion_buffer import MotionBufferCfg
-from dataclasses import MISSING
+from dataclasses import MISSING, field
 from typing import Union
 
 @configclass
 class MotionManagerCfg:
-    motion_buffer_cfg: MotionBufferCfg = MotionBufferCfg()
+    motion_buffer_cfg: MotionBufferCfg = field(default_factory=MotionBufferCfg)
     
     static_motion:      bool                = False
     loc_gen:            bool                = True
     speed_scale:        float               = 1.0
-    robot_type:         str                 = MISSING
+    robot_type:         str                 = ""
     motion_align_cfg:   Union[dict, str]    = None
     
     def set_motion_align_cfg(self, cfg):
